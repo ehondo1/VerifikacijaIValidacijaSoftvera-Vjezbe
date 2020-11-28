632d20f6f30319812932cd5e710587b0429ad380
sselmanovi1 <esmaimubineta123>
2020-11-28 23:45:09 +0100

    dodani testovi za klasu korisniik

---
+++ Kupid/Unit Testovi/UnitTestKorisnik.cs
@@ -0,0 +1,40 @@
+﻿
+using Kupid;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Unit_Testovi
+{ [TestClass]
+    public class UnitTestKorisnik
+    {
+        [TestMethod]
+        public void TestPromjenaParametaraSlicno()
+        {
+            Korisnik k1 = new Korisnik("Mubina", "Kamberovic", Lokacija.Sarajevo, Lokacija.Zenica, 21, false,19,29);
+            Korisnik k2 = new Korisnik("Nadija", "Borovina", Lokacija.Mostar, Lokacija.Bihać, 21, false,18,30);
+            bool slicno = true;
+            k1.PromjenaParametara(slicno);
+            Assert.AreEqual(k1.ZeljenaLokacija, k1.Lokacija);
+            
+        }
+
+        [TestMethod]
+        public void TestPromjenaParametaraSlicno1()
+        {
+            Korisnik k2 = new Korisnik("Nadija", "Borovina", Lokacija.Mostar, Lokacija.Bihać, 21, false, 18, 30);
+            bool slicno = false;
+            k2.PromjenaParametara(slicno);
+            Assert.AreEqual(k2.Lokacija,"Bihać");
+           
+        }
+        [TestMethod]
+        public void TestPromjenaParametaraSlicno2()
+        {
+            Korisnik k2 = new Korisnik("Nadija", "Borovina", Lokacija.Mostar, Lokacija.Bihać, 21, false, 18, 30);
+            bool slicno = false;
+            k2.PromjenaParametara(slicno);
+            Assert.AreEqual(k2.ZeljeniMaxGodina, 40);
+
+        }
+    }
+}
