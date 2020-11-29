84a99a0e6f5aa05db932c3fde85554d85f2f59cf
sselmanovi1 <esmaimubineta123>
2020-11-30 00:30:11 +0100

    dodani testovi za klasu recenzija

---
+++ Kupid/Unit Testovi/UnitTestRecenzija.cs
@@ -0,0 +1,24 @@
+﻿using Kupid;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Unit_Testovi
+{
+    [TestClass]
+    public class UnitTestRecenzija
+    {
+     
+
+        [TestMethod]
+        public void TestDajUtisak()
+        {
+            IRecenzija i = new Recenzija();
+            Assert.AreEqual(i.DajUtisak(), "Pozitivan");
+            
+        }
+
+       
+
+      
+    }
+}
