5ac1ede1386ab125cbe0adbc65c71740e619f1d1
tosmanagic1 <tosmanagic1@etf.unsa.ba>
2020-11-29 18:24:55 +0100

    Dodan test za konnstruktor za klasu Chat

--- Kupid/Unit Testovi/UnitTestChat.cs
+++ Kupid/Unit Testovi/UnitTestChat.cs
@@ -40,5 +40,18 @@ namespace Unit_Testovi
             Assert.AreEqual(chat.Poruke.Count,4);
 
         }
+
+        [TestMethod]
+        
+        public void TestKonstruktor()
+        {
+            Korisnik k1 = new Korisnik("user1", "user1*+", Lokacija.Sarajevo, Lokacija.Sarajevo, 20, false, 20, 18);
+            Korisnik k2 = new Korisnik("user2", "user2*+", Lokacija.Sarajevo, Lokacija.Sarajevo, 20, false, 30, 20);
+            Chat chat = new Chat(k1, k2);
+            Assert.IsFalse(chat.Poruke.Count == 1);
+
+        }
+
+
     }
 }
